abba9bde73297202cc597b248ebd89988fa3a1f0
janiltojack@yahoo.com.br <jackanilto>
2020-11-25 00:22:58 -0300

    Foram adicionados os  Botoes e DBGrid

--- Cadastros/UTratamentos.pas
+++ Cadastros/UTratamentos.pas
@@ -4,10 +4,19 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Vcl.StdCtrls, Vcl.Buttons,
+  Vcl.Grids, Vcl.DBGrids;
 
 type
   TFrmTRATAMENTOS = class(TForm)
+    GridGrupos: TDBGrid;
+    EditCOD: TEdit;
+    EditGRUPOS: TEdit;
+    Label1: TLabel;
+    btnNovo: TBitBtn;
+    BtnSalvar: TBitBtn;
+    btnEditar: TBitBtn;
+    btnDeletar: TBitBtn;
   private
     { Private declarations }
   public

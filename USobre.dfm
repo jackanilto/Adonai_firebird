94c59ca271dfde17d3a4a5c2588f2c6c666d64e5
janiltojack@yahoo.com.br <jackanilto>
2020-12-15 23:01:31 -0300

    Mostrar tempo restante da licensa de uso

--- USobre.dfm
+++ USobre.dfm
@@ -14,6 +14,26 @@ object FrmSobre: TFrmSobre
   Font.Style = []
   OldCreateOrder = False
   Position = poScreenCenter
+  OnActivate = FormActivate
   PixelsPerInch = 96
   TextHeight = 13
+  object Label1: TLabel
+    Left = 40
+    Top = 56
+    Width = 31
+    Height = 13
+    Caption = 'Label1'
+  end
+  object SpeedButton1: TSpeedButton
+    Left = 104
+    Top = 176
+    Width = 111
+    Height = 22
+  end
+  object EvAppProtect1: TEvAppProtect
+    IdApplication = 1974
+    RegKey = '\SOFTWARE\TCF Software\AppProtect\'
+    Left = 392
+    Top = 176
+  end
 end
--- USobre.pas
+++ USobre.pas
@@ -4,14 +4,20 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, EAppProt, Vcl.ExtCtrls,
+  Vcl.Buttons;
 
 type
   TFrmSobre = class(TForm)
+    Label1: TLabel;
+    EvAppProtect1: TEvAppProtect;
+    SpeedButton1: TSpeedButton;
+    procedure FormActivate(Sender: TObject);
   private
     { Private declarations }
   public
     { Public declarations }
+
   end;
 
 var
@@ -22,5 +28,13 @@ implementation
 {$R *.dfm}
 
 uses UPrincipal;
+
+
+procedure TFrmSobre.FormActivate(Sender: TObject);
+begin
+  Label1.Caption := 'Aplicação liberada para use de ' + #13 +
+    FormatDateTime('dd/mm/yyyy', EvAppProtect1.DateStart) + ' até ' +
+    FormatDateTime('dd/mm/yyyy', EvAppProtect1.DateEnd);
+end;
 
 end.

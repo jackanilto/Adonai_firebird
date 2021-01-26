3ca877da9ec5250b84219ded52a16e1239b46fb6
janiltojack@yahoo.com.br <jackanilto>
2021-01-26 01:50:50 -0300

    Config Form Entradas

--- Movimentacoes/UEntraSaida.pas
+++ Movimentacoes/UEntraSaida.pas
@@ -4,11 +4,16 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Vcl.Grids, Vcl.DBGrids;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Vcl.Grids, Vcl.DBGrids,
+  Vcl.StdCtrls, Vcl.Buttons;
 
 type
   TFormEntraSaida = class(TForm)
     DBGrid1: TDBGrid;
+    btnNovo: TBitBtn;
+    BtnSalvar: TBitBtn;
+    btnEditar: TBitBtn;
+    btnDeletar: TBitBtn;
   private
     { Private declarations }
   public
